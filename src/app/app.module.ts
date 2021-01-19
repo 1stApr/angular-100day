c5d461e47e5abbf1d68128df7da871f00901befe
1stApr <anhtuan01041998@gmail.com>
2021-01-19 22:08:37 +0700

    add directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { HelloComponent } from './hello.component';
 import { TabGroupComponent } from './tabs/tab-group.component';
 import { TabPanelComponent } from './tabs/tab-panel.component';
 import { TabBsGroupComponent } from './tabs/tab-bs-group.component';
+import { StackBarComponent } from './stack-bar/stack-bar.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { StackBarChartComponent } from './stack-bar-chart/stack-bar-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { StackBarDirective } from './stack-bar.directive';
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { TabBarComponent } from './tab-bar/tab-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,TabGroupComponent,TabPanelComponent,TabBsGroupComponent ],
+  declarations: [ AppComponent, HelloComponent,TabGroupComponent,TabPanelComponent,TabBsGroupComponent, StackBarComponent, BarChartComponent, StackBarChartComponent, PieChartComponent, StackBarDirective, LineChartComponent, TabBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/stack-bar-chart/stack-bar-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stack-bar-chart',
+  templateUrl: './stack-bar-chart.component.html',
+  styleUrls: ['./stack-bar-chart.component.css']
+})
+export class StackBarChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
